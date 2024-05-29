978e3cad21353a01519e3d964561a426b3772d88
20101301-Alina-Hasan <alina.hasan@g.bracu.ac.bd>
2024-05-29 22:39:14 +0600

    "Some minor updates."

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,7 +1,9 @@
 class Collection < ApplicationRecord
     belongs_to :user
-    has_many :items, dependent: :destroy
-    has_many :custom_details, dependent: :destroy
+    # has_many :items, dependent: :destroy
+    has_many :items
+    # has_many :custom_details, dependent: :destroy
+    has_many :custom_details
     belongs_to :category, optional: true
   
     validates :name, presence: true
